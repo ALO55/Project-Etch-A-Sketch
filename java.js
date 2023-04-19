2df20a56f57e892355b15b5038d4384d62de41fa
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-04-19 13:05:05 -0700

    I have added some buttons for the UI and right now the user can select the size of the grid

--- java.js
+++ java.js
@@ -1,21 +1,35 @@
 const container = document.querySelector('#container');
+const sizeSquares = document.querySelector('#sizeSquares');
 let selection = 16;
 
-for(let r = 0; r < selection; r++){
+function grid(selection){
+  for(let r = 0; r < selection; r++){
     const row = document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
 
-    for(let c= 0; c < selection; c++){
+    for(let r = 0; r < selection; r++){
       const column = document.createElement('div');
       column.classList.add('column');
       row.appendChild(column);
+
+      //this code is for coloring the boxes of the grid
+      const box = document.querySelectorAll('.column');
+      box.forEach((box) =>{
+      box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'black';
+        })
+      });
     }
+  }
 }
 
-const box = document.querySelectorAll('.column');
-box.forEach((box) =>{
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'black';
+grid(selection);
+
+  //this part let you choose the size of your grid
+  sizeSquares.addEventListener('click', () => {
+    document.getElementById('container').innerHTML = "";
+    selection = prompt ('Choose the size of your grid from 2 to 30 squares per side');
+    grid(selection);
     })
-});
+
